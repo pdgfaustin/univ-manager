479234ba3d6dbe5651af055a848aa43d1de72a92
Faustin Flory <pdgfaustin@gmail.com>
2025-06-22 15:07:05 +0100

    Initialisation du fichier package.json

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const sequelize = require("sequelize");
+const env = require("./index");
+console.log("🙇‍♀️ Initialisation de la connection mysql");;
+
+const db = new sequelize(env.DATABASE,env.USER,env.PASSWORD,{
+    host : env.HOST,
+    dialect:env.DIALECT,
+    port: env.PORT,
+    login:false
+});
+const connection = async () =>{
+    try {
+        console.log("🆗 tentative de connection ...");
+        await db.authenticate();
+        console.log("✅ Connection réussie");
+        
+    } catch (error) {
+        console.log("❌ Erreur : ",error);
+        
+    }
+}
+connection();
+module.exports = db;
